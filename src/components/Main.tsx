11e59ecf8978754d7988fcdbd0720cd6f7d9ff25
alexscott812 <alexscott@alexs-mbp-2.myfiosgateway.com>
2022-02-11 14:57:33 -0500

    added toggle theme functionality

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,42 @@
+import {
+  Heading,
+  Container,
+  Textarea,
+  Flex,
+  IconButton,
+  Spacer,
+  useColorMode
+} from "@chakra-ui/react";
+import {
+  SunIcon,
+  MoonIcon
+} from '@chakra-ui/icons';
+
 const Main = () => {
+
+  type UseColorModeProps = {
+    colorMode: 'light' | 'dark';
+    toggleColorMode: () => void;
+  };
+
+  const {
+    colorMode,
+    toggleColorMode
+  }: UseColorModeProps = useColorMode();
+
   return (
-    <header>local-storage-notes</header>
+    <Container maxW={ 'container.xl' } py={ 4 }>
+      <Flex align={ 'center' } justify={ 'center' } mb={ 4 }>
+        <Heading>{ 'local-storage-notes' }</Heading>
+        <Spacer />
+        <IconButton
+          aria-label={ 'toggle-color-mode' }
+          onClick={ toggleColorMode }
+          icon={ colorMode === 'light' ? <MoonIcon /> : <SunIcon /> }
+        />
+      </Flex>
+      <Textarea size={ 'lg' }/>
+    </Container>
   );
 };
 
